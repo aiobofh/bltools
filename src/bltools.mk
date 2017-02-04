e183c8ebe7e362f75dfac5246156fafcbcda7b57
Joakim Ekblad <aio.github@aio.nu>
2017-02-04 15:02:50 +0100

    Make use of MAKE environment variable instead of calling the binary directly

--- src/bltools.mk
+++ src/bltools.mk
@@ -1,16 +1,16 @@
 Q=@
 
 blsum: bltools
-	${Q}make -s -C bltools/src/ blsum; ln -s bltools/src/blsum
+	${Q}${MAKE} -s -C bltools/src/ blsum; ln -s bltools/src/blsum
 
 blcheck: bltools
-	${Q}make -s -C bltools/src/ blcheck; ln -s bltools/src/blcheck
+	${Q}${MAKE} -s -C bltools/src/ blcheck; ln -s bltools/src/blcheck
 
 blsprints: bltools
-	${Q}make -s -C bltools/src/ blsprints; ln -s bltools/src/blsprints
+	${Q}${MAKE} -s -C bltools/src/ blsprints; ln -s bltools/src/blsprints
 
 blburn: bltools
-	${Q}make -s -C bltools/src/ blburn; ln -s bltools/src/blburn
+	${Q}${MAKE} -s -C bltools/src/ blburn; ln -s bltools/src/blburn
 
 clean::
 	${Q}rm -rf blcheck blsum blsprints blburn
