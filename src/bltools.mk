6013445cd66c8bfb43de2c985a5d104b7effebfc
Joakim Ekblad <aio.github@aio.nu>
2017-02-04 14:20:18 +0100

    Add a helper .mk-file for use with your own backlog Makefile

---
+++ src/bltools.mk
@@ -0,0 +1,16 @@
+Q=@
+
+blsum: bltools
+	${Q}make -s -C bltools/src/ blsum; ln -s bltools/src/blsum
+
+blcheck: bltools
+	${Q}make -s -C bltools/src/ blcheck; ln -s bltools/src/blcheck
+
+blsprints: bltools
+	${Q}make -s -C bltools/src/ blsprints; ln -s bltools/src/blsprints
+
+blburn: bltools
+	${Q}make -s -C bltools/src/ blburn; ln -s bltools/src/blburn
+
+cleam::
+	${Q}rm -rf blcheck blsum blsprints blburn
